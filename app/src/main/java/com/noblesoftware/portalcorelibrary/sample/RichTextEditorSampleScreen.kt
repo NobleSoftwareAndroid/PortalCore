28bf00bf75c324b19e76831c09cb1fb70dade813
fizhu <hvyz.anbiya@gmail.com>
2025-12-03 09:31:40 +0700

    :hammer: update rich text editor

--- app/src/main/java/com/noblesoftware/portalcorelibrary/sample/RichTextEditorSampleScreen.kt
+++ app/src/main/java/com/noblesoftware/portalcorelibrary/sample/RichTextEditorSampleScreen.kt
@@ -1,10 +1,12 @@
 package com.noblesoftware.portalcorelibrary.sample
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -18,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.noblesoftware.portalcore.R
 import com.noblesoftware.portalcore.component.compose.ButtonType
